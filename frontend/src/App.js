52a443f1950f7db19bc8cd381fbe749c37983d45
pabloruizbarba <pruizb@fpcoruna.afundacion.org>
2023-05-02 13:02:32 +0200

    devices-menu finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import { Routes } from 'react-router';
 import NotFound from './screens/NotFound/NotFound';
 import Home from './screens/Home/Home';
+import DevicesMenu from './screens/DevicesMenu/DevicesMenu';
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />}></Route>
+      <Route path="/devices-menu" element={<DevicesMenu />}></Route>
       <Route path="/" element={<Home />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
