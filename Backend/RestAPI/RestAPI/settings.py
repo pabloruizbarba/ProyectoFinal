da6c51a04175e51f833814878f4554299583e48d
pablo <pablo@Ubuntu.myguest.virtualbox.org>
2023-05-11 13:27:27 +0200

    Backend: I changed the path to media folder

--- Backend/RestAPI/RestAPI/settings.py
+++ Backend/RestAPI/RestAPI/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-#import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,7 +154,3 @@ CORS_ALLOW_HEADERS = [
 ]
 
 
-#DIRECTORY WHERE MEDIA FILES WILL BE STORED
-#MEDIA_URL = '../../media/' 
-
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'webserviceapp', 'media') 
--- Backend/RestAPI/RestAPI/urls.py
+++ Backend/RestAPI/RestAPI/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import webserviceapp.views as views
-#from django.conf import settings
-#from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -43,5 +42,3 @@ urlpatterns = [
     path('v1/add-code/', views.add_code),
 ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
